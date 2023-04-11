a8d34c2a4a1adf697ba60d1381261ca742ae7a70
Zack F <zackaryfotheringham@hotmail.com>
2023-04-11 07:28:21 -0400

    add custom tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "toshi-red": "#a91101",
+        "web-white": "#e3e6e6",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
