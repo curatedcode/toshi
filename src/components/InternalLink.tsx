c86993380818373a4224e9944554a3e4afb5f9e4
Zack F <zackaryfotheringham@hotmail.com>
2023-05-18 23:30:40 -0400

    fix ref not working on InternalLink component

--- src/components/InternalLink.tsx
+++ src/components/InternalLink.tsx
@@ -1,30 +1,33 @@
 import Link from "next/link";
+import { forwardRef } from "react";
 import type { InternalLinkProps } from "~/customTypes";
 
-function InternalLink({
-  href,
-  className = "",
-  children,
-  ...props
-}: InternalLinkProps) {
-  if (children)
+const InternalLink = forwardRef<HTMLAnchorElement, InternalLinkProps>(
+  function InternalLink(
+    { href, className = "", children, ...props }: InternalLinkProps,
+    ref
+  ) {
+    if (children)
+      return (
+        <Link
+          href={href}
+          className={`w-fit text-base text-sky-600 underline underline-offset-1 transition-colors hover:text-toshi-red ${className}`}
+          ref={ref}
+          {...props}
+        >
+          {children}
+        </Link>
+      );
+
     return (
       <Link
         href={href}
         className={`w-fit text-base text-sky-600 underline underline-offset-1 transition-colors hover:text-toshi-red ${className}`}
+        ref={ref}
         {...props}
-      >
-        {children}
-      </Link>
+      />
     );
-
-  return (
-    <Link
-      href={href}
-      className={`w-fit text-base text-sky-600 underline underline-offset-1 transition-colors hover:text-toshi-red ${className}`}
-      {...props}
-    />
-  );
-}
+  }
+);
 
 export default InternalLink;
