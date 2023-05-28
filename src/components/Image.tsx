9e9ecfd6693c06fa5e985840b747354c2fb33d53
Zack F <zackaryfotheringham@hotmail.com>
2023-05-28 01:46:15 -0400

    fix layout shift from images loading added loading animation

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,9 +1,32 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
+import { useState } from "react";
 import type { ImageProps } from "~/customTypes";
 
-function Image(props: ImageProps) {
-  return <img loading={props.loading ? props.loading : "lazy"} {...props} />;
+function Image({ className = "", ...props }: ImageProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <>
+      <img
+        {...props}
+        loading={props.loading ? props.loading : "lazy"}
+        onLoad={() => setIsLoading(false)}
+        className={`${isLoading ? "w-0" : ""} ${className}`}
+      />
+      <div className={`relative ${isLoading ? "" : "hidden"}`}>
+        <img
+          {...props}
+          loading={props.loading ? props.loading : "lazy"}
+          className={`${isLoading ? "" : "w-0"} ${className}`}
+          src="/product-placeholder.png"
+        />
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="h-8 w-8 animate-spin rounded-full border-4 border-toshi-primary border-r-transparent align-[-0.125em]" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Image;
