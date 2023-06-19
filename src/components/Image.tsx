7ae374c20a256e0f5ec20444b931151b11ee1479
Zack F <zackaryfotheringham@hotmail.com>
2023-06-19 02:01:52 -0400

    fix images stuck loading occasionally on refresh we were not checking if image was already loaded

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,14 +1,23 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { ImageProps } from "~/customTypes";
 
 function Image({ className = "", ...props }: ImageProps) {
   const [isLoading, setIsLoading] = useState(true);
+
+  const ref = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (ref.current?.complete) {
+      setIsLoading(false);
+    }
+  }, [isLoading]);
   return (
     <>
       <img
         {...props}
+        ref={ref}
         onLoad={() => setIsLoading(false)}
         onError={(el) => (el.currentTarget.src = "/image-fallback.png")}
         className={`${isLoading ? "w-0 max-w-0" : ""} ${className}`}
